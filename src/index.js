0273d496053104b346718bfd6f31c2ad3bb9cca5
franciscotimez <63823155+franciscotimez@users.noreply.github.com>
2021-11-22 15:06:25 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ const receiptsApi = require('./routes/receipt');
 
 app.get('/', (req, res) => {
   let userInfo = req.header("user-agent");
-  res.send(`UserInfo: ${userInfo}`);
+  res.send(`Dale javi metete en el mundo IT`);
 });
 
 productsApi(app);
